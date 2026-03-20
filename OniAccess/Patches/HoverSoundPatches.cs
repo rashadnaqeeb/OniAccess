cccfc82d4886cb56867bf70896e7770682b13ba0
Rashad Naqeeb <rashadnaqeeb@gmail.com>
2026-03-20 20:43:36 +0300

    Move pause snapshot patch to its own file

--- OniAccess/Patches/HoverSoundPatches.cs
+++ OniAccess/Patches/HoverSoundPatches.cs
@@ -40,11 +40,4 @@ namespace OniAccess.Patches {
 		}
 	}
 
-	[HarmonyPatch(typeof(SpeedControlScreen), nameof(SpeedControlScreen.Pause))]
-	internal static class SpeedControlScreen_Pause_Patch {
-		static void Postfix() {
-			AudioMixer.instance.Stop(AudioMixerSnapshots.Get().SpeedPausedMigrated,
-				FMOD.Studio.STOP_MODE.IMMEDIATE);
-		}
-	}
 }
